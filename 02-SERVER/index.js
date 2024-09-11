4266eb765451892d72777c08bd250b7983beea55
Vaibhav Verma <vaibhavverma3231@gmail.com>
2024-09-12 03:48:10 +0530

    07 HTTP Methods

--- 02-SERVER/index.js
+++ 02-SERVER/index.js
@@ -8,7 +8,7 @@ const myServer = http.createServer((req, res) => {
     // console.log(req);
     // console.log(req.headers);
 
-    const log = `${Date.now()}: ${req.url} New Req Received\n`;
+    const log = `${Date.now()}: ${req.method} ${req.url} New Req Received\n`;
 
     const myUrl = url.parse(req.url, true);
     console.log(myUrl);
@@ -18,6 +18,7 @@ const myServer = http.createServer((req, res) => {
 
         switch(myUrl.pathname){
             case '/': 
+                if(req.method === "GET")
                 res.end("HomePage");
                 break
             case '/about': 
@@ -27,6 +28,13 @@ const myServer = http.createServer((req, res) => {
             case "/search":
                 const search = myUrl.query.search_query;
                 res.end(`Here are the results for ${search}`);
+                break
+            case "/signup":
+                if(req.method === "GET")  res.end("This is a Signup Form");
+                else if(req.method === "POST"){
+                    // DB Query
+                    res.end("Success");
+                }
                 break
             default:
                 res.end("404 Not Found");
