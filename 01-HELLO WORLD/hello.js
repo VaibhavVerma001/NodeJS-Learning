8f9c5ccb5a9bb2458cabbaf6557fc7d056f34e2e
Vaibhav Verma <vaibhavverma3231@gmail.com>
2024-09-11 16:33:15 +0530

    02 Modules

--- 01-HELLO WORLD/hello.js
+++ 01-HELLO WORLD/hello.js
@@ -1,1 +1,18 @@
-console.log("Hey there! I am JS");
+// const math = require('./math');
+const {add, sub} = require('./math');
+
+// console.log(math(2,4));
+// console.log(math);
+
+// console.log(math.subFn(4,2));
+// console.log(math.addFn(4,3));
+
+// console.log(math.add(4,2));
+// console.log(math.sub(4,3));
+
+console.log(add(4,2));
+console.log(sub(4,3));
+
+
+
+
---
+++ 01-HELLO WORLD/math.js
@@ -0,0 +1,21 @@
+function add(a, b){
+    return a + b;
+}
+
+function sub(a,b){
+    return a - b; 
+}
+
+// exports.add1 = (a,b) => a+b;
+// exports.sub2 = (a,b) => a-b;
+
+module.exports = {
+    add,
+    sub
+}
+
+// module.exports = {
+//     addFn: add,
+//     subFn: sub
+// }
+
